3ac0a1b388d0b926beca14d73fed72df884b4848
Daniele <daniele.rossi18@studio.unibo.it>
2021-10-14 08:47:38 -0700

    Add some new test and test refactoring.

--- swagger_server/test/MEC011_service_management/test_app_services_controller.py
+++ swagger_server/test/MEC011_service_management/test_app_services_controller.py
@@ -4,37 +4,18 @@ from __future__ import absolute_import
 
 from flask import json
 
-from swagger_server.models.MEC011_service_management.end_point_info_address import EndPointInfoAddress
-from swagger_server.models.MEC011_service_management.end_point_info_addresses import EndPointInfoAddresses
-from swagger_server.models.MEC011_service_management.security_info import SecurityInfo
 from swagger_server.models.MEC011_service_management.service_info import ServiceInfo  # noqa: E501
-from swagger_server.models.MEC011_service_management.service_info_post import ServiceInfoPost  # noqa: E501
-from swagger_server.models.MEC011_service_management.transport_info import TransportInfo
-from swagger_server.test import BaseTestCase
+from swagger_server.test import BaseTestCase, APP_INSTANCE_ID
 
-
-def get_service_info_post():
-	return ServiceInfoPost(ser_name="name", serializer="JSON", state="ACTIVE", version="2.0.0",
-	                       consumed_local_only=True, is_local=True,
-	                       transport_info=TransportInfo(id="TransId12345", name="REST", type="REST_HTTP",
-	                                                    protocol="HTTP", version="2.0",
-	                                                    endpoint=EndPointInfoAddresses(
-		                                                    [EndPointInfoAddress(host="localhost", port=2234)]),
-	                                                    security=SecurityInfo()))
+from swagger_server.test.MEC011_service_management.utils.test_utilities import get_service_info_post
+from swagger_server.test.MEC011_service_management.utils.test_utilities import SERVICE_INFORMATIONS_TEMPLATE, SERVICE_ID
 
 
 class TestAppServicesController(BaseTestCase):
-	"""AppServicesController integration test stubs"""
+	"""AppServicesController integration test."""
 
-	def add_service(self, app_instance_id, service_info):
-		return self.client.post(
-			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id),
-			data=json.dumps(service_info),
-			content_type='application/json')
-
-	def test_app_services_get_empty_list(self):
-		"""Test case for app_services_get"""
+	def test_app_services_fail(self):
+		"""Test case for app_services_get. Should return 404 because the application is not in READY state."""
 
 		query_string = [('ser_instance_id', 'ser_instance_id_example'),
 		                ('ser_name', 'ser_name_example'),
@@ -45,156 +26,163 @@ class TestAppServicesController(BaseTestCase):
 
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId='app_instance_id_example'),
+				appInstanceId=APP_INSTANCE_ID),
 			query_string=query_string)
-		self.assert200(response,
-		               'Response body is : ' + response.data.decode('utf-8'))
-		self.assertTrue(len(response.json) == 0)
+		self.check_status_code(status_expected=404, response=response,
+		                       message="Should return 404 because the application is not in READY state.")
 
 	def test_app_services_get(self):
-		"""Test case for app_services_get"""
+		"""Test case for app_services_get. No filter query is passed in the request. Should return 200.
+        The body contains the SERVICE_INFORMATIONS data with the addition of serInstanceId."""
 
-		app_instance_id = 'app_instance_id_example'
-		self.add_service(app_instance_id=app_instance_id, service_info=get_service_info_post())
+		self.register_app_and_add_service()
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id))
-		self.assertTrue(len(response.json) == 1)
-		self.assert200(response,
-		               'Response body is : ' + response.data.decode('utf-8'))
+				appInstanceId=APP_INSTANCE_ID))
+		self.check_status_code(200, response)
+		response_body_data = response.json
+		self.assertTrue(len(response_body_data) == 1)
+		self.check_returned_service_informations(response_body_data[0])
 
 	def test_app_services_get_with_service_name_filter(self):
-		"""Test case for app_services_get"""
+		"""Test case for app_services_get. Filter query is passed in the request and match the previous inserted service.
+        Should return 200. The body contains the SERVICE_INFORMATIONS data with the addition of serInstanceId."""
 
-		app_instance_id = 'app_instance_id_example'
-		self.add_service(app_instance_id, get_service_info_post())
+		self.register_app_and_add_service()
 		query_string = [('ser_name', 'name')]
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id),
+				appInstanceId=APP_INSTANCE_ID),
 			query_string=query_string)
-		self.assertTrue(len(response.json) == 1)
-		self.assert200(response,
-		               'Response body is : ' + response.data.decode('utf-8'))
+		self.check_status_code(200, response)
+		response_body_data = response.json
+		self.assertTrue(len(response_body_data) == 1)
+		self.check_returned_service_informations(response_body_data[0])
 
 	def test_app_services_get_with_service_name_filter_empty(self):
-		"""Test case for app_services_get"""
+		"""Test case for app_services_get. Filter query is passed in the request and match the previous inserted service.
+        Should return 200 with an empty body."""
 
-		app_instance_id = 'app_instance_id_example'
-		self.add_service(app_instance_id, get_service_info_post())
+		self.register_app_and_add_service()
 		query_string = [('ser_name', 'name1')]
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id),
+				appInstanceId=APP_INSTANCE_ID),
 			query_string=query_string)
+		self.check_status_code(200, response)
 		self.assertTrue(len(response.json) == 0)
-		self.assert200(response,
-		               'Response body is : ' + response.data.decode('utf-8'))
 
 	def test_app_services_get_with_combined_filters(self):
-		"""Test case for app_services_get"""
+		"""Test case for app_services_get. Multiple filter query is passed in the request and match the previous
+        inserted service. Should return 200. The body contains the SERVICE_INFORMATIONS data
+        with the addition of serInstanceId."""
 
-		app_instance_id = 'app_instance_id_example'
-		self.add_service(app_instance_id, get_service_info_post())
+		self.register_app_and_add_service()
 		query_string = [('ser_name', 'name'), ('consumed_local_only', True),
 		                ('is_local', True)]
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id),
+				appInstanceId=APP_INSTANCE_ID),
 			query_string=query_string)
-		self.assertTrue(len(response.json) == 1)
-		self.assert200(response,
-		               'Response body is : ' + response.data.decode('utf-8'))
+		self.check_status_code(200, response)
+		response_body_data = response.json
+		self.assertTrue(len(response_body_data) == 1)
+		self.check_returned_service_informations(response_body_data[0])
+
+	def test_app_services_post_fail(self):
+		"""Test case for app_services_post. Should return 404 because the application is not in READY state."""
+
+		response = self.add_service(service_info=get_service_info_post(), app_instance_id=APP_INSTANCE_ID)
+		self.check_status_code(404, response,
+		                       message="Should return 404 because the application is not in READY state.")
 
 	def test_app_services_post(self):
-		"""Test case for app_services_post"""
-
-		response = self.client.post(
-			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId='app_instance_id_example'),
-			data=json.dumps(get_service_info_post()),
-			content_type='application/json')
+		"""Test case for app_services_post. Should return 201. The body contains the SERVICE_INFORMATIONS data with
+        the addition of serInstanceId."""
+		response = self.register_app_and_add_service()
 		response_body_data = response.json
-		self.assertTrue(response.status_code == 201)
-		self.assertTrue('serInstanceId' in response_body_data and
-		                response_body_data['serName'] == 'name' and
-		                response_body_data['serializer'] == "JSON" and response_body_data['state'] == "ACTIVE" and
-		                response_body_data['version'] == "2.0.0")
+		self.check_status_code(201, response)
+		self.check_returned_service_informations(response_body_data)
 
 	def test_app_services_service_id_delete_empty(self):
-		"""Test case for app_services_service_id_delete"""
+		"""Test case for app_services_service_id_delete. Should return 404 because the application is in READY state,
+        but there is no service registered."""
+		self.app_ready(app_instance_id=APP_INSTANCE_ID)
 		response = self.client.delete(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-				appInstanceId='app_instance_id_example', serviceId='service_id_example'))
-		self.assert404(response)
+				appInstanceId=APP_INSTANCE_ID, serviceId=SERVICE_ID))
+		self.check_status_code(404, response,
+		                       message="Should return 404 because the application is in READY state, "
+		                               "but there is no service registered.")
 
 	def test_app_services_service_id_delete(self):
-		"""Test case for app_services_service_id_delete"""
+		"""Test case for app_services_service_id_delete.Should return 204 because the application is in READY state and
+        there is a service registered that match the specified id."""
 
-		app_instance_id = 'app_instance_id_example'
-		added_service = self.add_service(app_instance_id, get_service_info_post())
+		added_service = self.register_app_and_add_service()
 
 		response = self.client.delete(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-				appInstanceId=app_instance_id, serviceId=added_service.json['serInstanceId']))
+				appInstanceId=APP_INSTANCE_ID, serviceId=added_service.json['serInstanceId']))
 
-		self.assertTrue(response.status_code == 204)
+		self.check_status_code(204, response)
 
 	def test_app_services_service_id_get_not_found(self):
-		"""Test case for app_services_service_id_get"""
+		"""Test case for app_services_service_id_get. Should return 404 because the application is in READY state,
+        but there is no service registered."""
+		self.app_ready(app_instance_id=APP_INSTANCE_ID)
 		response = self.client.get('/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-			appInstanceId='app_instance_id_example', serviceId='service_id_example'))
-		self.assert404(response)
+			appInstanceId=APP_INSTANCE_ID, serviceId='service_id_example'))
+		self.check_status_code(404, response,
+		                       "Should return 404 because the application is in READY state, but there "
+		                       "is no service registered.")
 
 	def test_app_services_service_id_get(self):
-		"""Test case for app_services_service_id_get"""
+		"""Test case for app_services_service_id_get. Should return 200 because the application is in READY state and
+        there is a service registered that match the specified id. The body contains the SERVICE_INFORMATIONS data with
+        the addition of serInstanceId."""
 
-		app_instance_id = 'app_instance_id_example'
-		added_service = self.add_service(app_instance_id, get_service_info_post())
+		added_service = self.register_app_and_add_service()
 
 		response = self.client.get(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-				appInstanceId=app_instance_id, serviceId=added_service.json['serInstanceId']))
-		self.assert200(response)
+				appInstanceId=APP_INSTANCE_ID, serviceId=added_service.json['serInstanceId']))
+		self.check_status_code(200, response)
+		response_body_data = response.json
+		self.check_returned_service_informations(response_body_data)
 
 	def test_app_services_service_id_put_not_found(self):
-		"""Test case for app_services_service_id_put"""
+		"""Test case for app_services_service_id_put. Should return 404 because the application is in READY state,
+        but there is no service registered."""
 
-		body = ServiceInfo(ser_name="name", serializer="JSON", version="ServiceVersion1", state="ACTIVE",
-		                   transport_info=TransportInfo(id="TransId12345", name="REST", type="REST_HTTP",
-		                                                protocol="HTTP", version="2.0",
-		                                                endpoint=EndPointInfoAddresses(
-			                                                [EndPointInfoAddress(host="localhost", port=2234)]),
-		                                                security=SecurityInfo()))
+		self.app_ready(app_instance_id=APP_INSTANCE_ID)
+		body = ServiceInfo.from_dict(SERVICE_INFORMATIONS_TEMPLATE)
 		response = self.client.put(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-				appInstanceId='app_instance_id_example', serviceId='service_id_example'),
+				appInstanceId=APP_INSTANCE_ID, serviceId=SERVICE_ID),
 			data=json.dumps(body),
 			content_type='application/json')
-		self.assert404(response)
+		self.check_status_code(404, response,
+		                       message="Should return 404 because the application is in READY state, "
+		                               "but there is no service registered.")
 
 	def test_app_services_service_id_put(self):
-		"""Test case for app_services_service_id_put"""
+		"""Test case for app_services_service_id_put. Should return 200 because the application is in READY state and
+        there is a service registered that match the specified id. The body contains the updated service
+        informations."""
 
-		body = get_service_info_post()
-		app_instance_id = 'app_instance_id_example'
-		added_service_response = self.client.post(
-			'/mec_service_mgmt/v1/applications/{appInstanceId}/services'.format(
-				appInstanceId=app_instance_id),
-			data=json.dumps(body),
-			content_type='application/json')
-
-		added_service = ServiceInfo.from_dict(added_service_response.json)
-		added_service.ser_name = "name1"
+		added_service_response = self.register_app_and_add_service()
+		new_service_info = SERVICE_INFORMATIONS_TEMPLATE.copy()
+		new_service_info['serName'] = 'new_name'
 
 		response = self.client.put(
 			'/mec_service_mgmt/v1/applications/{appInstanceId}/services/{serviceId}'.format(
-				appInstanceId=app_instance_id, serviceId=added_service.ser_instance_id),
-			data=json.dumps(body),
+				appInstanceId=APP_INSTANCE_ID, serviceId=added_service_response.json['serInstanceId']),
+			data=json.dumps(new_service_info),
 			content_type='application/json')
 
-		self.assert200(response)
-		self.assertTrue(response.json['serName'] == body.ser_name)
+		self.check_status_code(200, response)
+		self.assertTrue(response.json['serName'] == new_service_info['serName'])
 
 
 if __name__ == '__main__':
